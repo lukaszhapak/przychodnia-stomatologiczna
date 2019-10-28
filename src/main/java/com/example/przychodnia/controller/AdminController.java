9314a08feb0ee6b5dd83b7be2557137d3da87aa9
lhapak <hapak.lukasz@gmail.com>
2019-10-28 19:49:48 +0100

    contact data in admin panel is now queried from data base

--- src/main/java/com/example/przychodnia/controller/AdminController.java
+++ src/main/java/com/example/przychodnia/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.example.przychodnia.controller;
 
-import com.example.przychodnia.entity.ContactData;
+import com.example.przychodnia.service.ContactDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AdminController {
 
+    final private ContactDataService contactDataService;
+
+    @Autowired
+    public AdminController(ContactDataService contactDataService) {
+        this.contactDataService = contactDataService;
+    }
+
     @GetMapping("/admin")
     public String admin(Model model) {
-        model.addAttribute(new ContactData());
+        model.addAttribute(contactDataService.findAll().get(0));
         return "admin";
     }
 }

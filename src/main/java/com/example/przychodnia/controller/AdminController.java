35c074c5d23760379fed3a85e67a4680a0477848
lhapak <hapak.lukasz@gmail.com>
2019-10-28 20:21:50 +0100

    post method in admin panel

--- src/main/java/com/example/przychodnia/controller/AdminController.java
+++ src/main/java/com/example/przychodnia/controller/AdminController.java
@@ -1,10 +1,13 @@
 package com.example.przychodnia.controller;
 
+import com.example.przychodnia.entity.ContactData;
 import com.example.przychodnia.service.ContactDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AdminController {
@@ -20,5 +23,11 @@ public class AdminController {
     public String admin(Model model) {
         model.addAttribute(contactDataService.findAll().get(0));
         return "admin";
+    }
+
+    @PostMapping("/admin/updateContactData/{id}")
+    public String updateContactData(ContactData contactData, @PathVariable String id) {
+        contactDataService.save(contactData);
+        return "redirect:/admin";
     }
 }

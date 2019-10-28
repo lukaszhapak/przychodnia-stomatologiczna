3bf4c99fa33893e728d62c6e5658bbb08d238200
lhapak <hapak.lukasz@gmail.com>
2019-10-28 19:32:49 +0100

    contact data form in admin panel

--- src/main/java/com/example/przychodnia/controller/AdminController.java
+++ src/main/java/com/example/przychodnia/controller/AdminController.java
@@ -1,13 +1,16 @@
 package com.example.przychodnia.controller;
 
+import com.example.przychodnia.entity.ContactDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
 
     @GetMapping("/admin")
-    public String admin() {
+    public String admin(Model model) {
+        model.addAttribute(new ContactDetails());
         return "admin";
     }
 }

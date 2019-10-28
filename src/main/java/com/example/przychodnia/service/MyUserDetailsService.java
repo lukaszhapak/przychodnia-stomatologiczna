3d4123b7853ccff583f6bb099cc6a3ea429448c2
lhapak <hapak.lukasz@gmail.com>
2019-10-28 18:32:31 +0100

    user details service

---
+++ src/main/java/com/example/przychodnia/service/MyUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.example.przychodnia.service;
+
+import com.example.przychodnia.entity.MyUserDetails;
+import com.example.przychodnia.entity.User;
+import com.example.przychodnia.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    final private UserRepository userRepository;
+
+    @Autowired
+    public MyUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(userName);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("User not Found: " + userName));
+
+        return user.map(MyUserDetails::new).get();
+    }
+}

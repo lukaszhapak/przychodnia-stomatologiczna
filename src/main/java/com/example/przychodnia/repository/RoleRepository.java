89cb75a8ceaab01e9370e924233d469b6a4af412
lhapak <hapak.lukasz@gmail.com>
2019-10-29 19:49:39 +0100

    role repository

---
+++ src/main/java/com/example/przychodnia/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.example.przychodnia.repository;
+
+import com.example.przychodnia.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}

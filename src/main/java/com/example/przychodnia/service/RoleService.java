7e6500b1e1190444b02924920e7f9c0421715fc6
lhapak <hapak.lukasz@gmail.com>
2019-10-29 20:31:19 +0100

    new method in role repository

--- src/main/java/com/example/przychodnia/service/RoleService.java
+++ src/main/java/com/example/przychodnia/service/RoleService.java
@@ -5,6 +5,8 @@ import com.example.przychodnia.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class RoleService {
 
@@ -21,5 +23,9 @@ public class RoleService {
 
     public long count() {
         return roleRepository.count();
+    }
+
+    public Optional<Role> findByRoleName(String roleName) {
+        return roleRepository.findByRoleName(roleName);
     }
 }

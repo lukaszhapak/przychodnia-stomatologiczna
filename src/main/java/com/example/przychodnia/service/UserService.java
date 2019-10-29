cbfc544c55575d98a492de15d1b9ccbf216508f3
lhapak <hapak.lukasz@gmail.com>
2019-10-29 19:23:05 +0100

    user service

---
+++ src/main/java/com/example/przychodnia/service/UserService.java
@@ -0,0 +1,21 @@
+package com.example.przychodnia.service;
+
+import com.example.przychodnia.entity.User;
+import com.example.przychodnia.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
+}

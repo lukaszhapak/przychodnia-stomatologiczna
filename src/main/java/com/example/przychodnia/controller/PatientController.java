7a626e108072a82dfa1fe984d50c4f62f004d04e
lhapak <lukaszrdw@gmail.com>
2020-03-29 22:20:08 +0200

    template for visit registration

--- src/main/java/com/example/przychodnia/controller/PatientController.java
+++ src/main/java/com/example/przychodnia/controller/PatientController.java
@@ -1,15 +1,27 @@
 package com.example.przychodnia.controller;
 
+import com.example.przychodnia.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/patient")
+@RequiredArgsConstructor
 public class PatientController {
+
+    private final UserService userService;
 
     @GetMapping("")
     public String patient() {
         return "patient/patient";
+    }
+
+    @GetMapping("/visit/register")
+    public String visitRegistration(Model model) {
+        model.addAttribute("dentists", userService.findAllDentists());
+        return "patient/visit/register";
     }
 }

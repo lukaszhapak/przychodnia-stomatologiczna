b12357cedbfe051e7d81c8f35cd71d362aee6ab5
b.jakubowski <bartakj141@wp.pl>
2020-04-17 06:28:34 +0200

    add schedule service to dentistcontroller

--- src/main/java/com/example/przychodnia/controller/DentistController.java
+++ src/main/java/com/example/przychodnia/controller/DentistController.java
@@ -1,8 +1,11 @@
 package com.example.przychodnia.controller;
 
 import com.example.przychodnia.entity.MyUserDetails;
+import com.example.przychodnia.entity.Schedule;
 import com.example.przychodnia.entity.User;
+import com.example.przychodnia.repository.ScheduleRepository;
 import com.example.przychodnia.service.ContactDataService;
+import com.example.przychodnia.service.ScheduleService;
 import com.example.przychodnia.service.UserService;
 import com.example.przychodnia.service.VisitsCalendarService;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -27,6 +31,12 @@ public class DentistController {
     private final UserService userService;
     private final VisitsCalendarService visitsCalendarService;
     private final ContactDataService contactDataService;
+    private final ScheduleService scheduleService;
+
+    private String getCurrentUser(@AuthenticationPrincipal MyUserDetails currentUser){
+        Optional<User> user = userService.findByUserName(currentUser.getUsername());
+        return user.get().getId().toString();
+    }
 
     @GetMapping("")
     public String dentist() {
@@ -44,9 +54,9 @@ public class DentistController {
     public String getCalendarByWeek(@AuthenticationPrincipal MyUserDetails currentUser,@RequestParam String inputDateString, Model model){
         LocalTime open = contactDataService.findAll().get(0).getOpen();
         int defoultTimeVisit = 30;
-        Optional<User> user = userService.findByUsername(currentUser.getUsername());
-        String id_user = user.get().getId().toString();
-        model.addAttribute("visits", visitsCalendarService.findByDoctorId(id_user));
+        //Optional<User> user = userService.findByUsername(currentUser.getUsername());
+        //String id_user = user.get().getId().toString();
+        model.addAttribute("visits", visitsCalendarService.findByDoctorId(getCurrentUser(currentUser)));
         String [] inputDateTab = inputDateString.split("-");
         LocalDateTime date = LocalDateTime.of(Integer.parseInt(inputDateTab[0]),Integer.parseInt(inputDateTab[1]),Integer.parseInt(inputDateTab[2]),0,0);
         model.addAttribute("today", date);
@@ -55,5 +65,11 @@ public class DentistController {
         model.addAttribute("difrence", defoultTimeVisit);
         model.addAttribute(inputDateString);
         return "dentist/calendar/calendar";
+    }
+
+    @GetMapping("/schedule")
+    public String scheduleWeek(@AuthenticationPrincipal MyUserDetails currentUser, Model model){
+        model.addAttribute("schedules",scheduleService.findAllByDoctorId(getCurrentUser(currentUser)));
+        return "dentist/schedule";
     }
 }

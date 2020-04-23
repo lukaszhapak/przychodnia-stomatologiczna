7b219fd80fb8fb899ad063a4fce489499c364a50
b.jakubowski <bartakj141@wp.pl>
2020-04-23 16:38:40 +0200

    add method findFreeDayWeek in schedule service

--- src/main/java/com/example/przychodnia/repository/ScheduleRepository.java
+++ src/main/java/com/example/przychodnia/repository/ScheduleRepository.java
@@ -1,9 +1,16 @@
 package com.example.przychodnia.repository;
 
 import com.example.przychodnia.entity.Schedule;
+import com.example.przychodnia.entity.VisitsCalendar;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
     Iterable<Schedule> findAllByDoctorId(Long id);
+
+    @Query(value = "SELECT * FROM przychodnia_wspa.schedule where YEARWEEK(date) = YEARWEEK(:inputDate) and doctor_id = :doctor_id",
+                nativeQuery = true)
+    Iterable<Schedule> findWeekByDate(@Param("inputDate") String date, Long doctor_id);
 }
--- src/main/java/com/example/przychodnia/service/ScheduleService.java
+++ src/main/java/com/example/przychodnia/service/ScheduleService.java
@@ -5,6 +5,9 @@ import com.example.przychodnia.repository.ScheduleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,22 +17,50 @@ public class ScheduleService {
 
     private final ScheduleRepository scheduleRepository;
 
-    public List<Schedule> findAll(){
+    public List<Schedule> findAll() {
         return scheduleRepository.findAll();
     }
 
-    public Iterable<Schedule> findAllByDoctorId(String id){
+    public Iterable<Schedule> findAllByDoctorId(String id) {
         return scheduleRepository.findAllByDoctorId(Long.parseLong(id));
     }
-    public Optional<Schedule> findById(String id){
+
+    public Optional<Schedule> findById(String id) {
         return scheduleRepository.findById(Long.parseLong(id));
     }
 
-    public void addSchedule(Schedule schedule){
+    public Iterable<Schedule> findWeekByDay(String date, Long doctorId) {
+        return scheduleRepository.findWeekByDate(date, doctorId);
+    }
+
+    public List<LocalDate> findFreeDayInWeek(LocalDateTime date, Long doctorId) {
+        List<LocalDate> freeDayList = new ArrayList<>();
+        List<Schedule> dayOnSchedule = (List<Schedule>) scheduleRepository.findWeekByDate(date.toString(), doctorId);
+        int dayOfWeek = date.getDayOfWeek().getValue();
+        LocalDate firstDay = LocalDate.of(date.getYear(), date.getMonth(), date.getDayOfMonth());
+        firstDay = firstDay.minusDays(dayOfWeek);
+        LocalDate startDay = LocalDate.of(firstDay.getYear(), firstDay.getMonth(), firstDay.getDayOfMonth());
+        for (int i = 0; i < 7; i++) {
+            freeDayList.add(startDay.plusDays(i + 1));
+        }
+
+        for (int i = 0; i < dayOnSchedule.size(); i++) {
+            LocalDate temp = date.toLocalDate();
+            for (LocalDate item : freeDayList) {
+                if (dayOnSchedule.get(i).getDate().equals(item)) {
+                    temp = item;
+                }
+            }
+            freeDayList.remove(temp);
+        }
+        return freeDayList;
+    }
+
+    public void addSchedule(Schedule schedule) {
         scheduleRepository.save(schedule);
     }
 
-    public void deleteScheduleById(String id){
+    public void deleteScheduleById(String id) {
         scheduleRepository.deleteById(Long.parseLong(id));
     }
 }

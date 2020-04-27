183af8eb4d9d24c15b02d31f7c944fcb672c1dff
b.jakubowski <bartakj141@wp.pl>
2020-04-27 13:46:38 +0200

    add receptionist add visit method

--- src/main/java/com/example/przychodnia/controller/ReceptionistController.java
+++ src/main/java/com/example/przychodnia/controller/ReceptionistController.java
@@ -1,13 +1,20 @@
 package com.example.przychodnia.controller;
 
 import com.example.przychodnia.entity.Message;
+import com.example.przychodnia.entity.MyUserDetails;
+import com.example.przychodnia.entity.VisitsCalendar;
+import com.example.przychodnia.service.ContactDataService;
 import com.example.przychodnia.service.MessageService;
+import com.example.przychodnia.service.UserService;
+import com.example.przychodnia.service.VisitsCalendarService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Controller
 @RequestMapping("/receptionist")
@@ -15,6 +22,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ReceptionistController {
 
     private final MessageService messageService;
+    private final UserService userService;
+    private final ContactDataService contactDataService;
+    private final VisitsCalendarService visitsCalendarService;
+
+    private LocalDateTime getDateFromInputDate(String inputDate) {
+        String[] inputDateTab = inputDate.split("-");
+        return LocalDateTime.of(Integer.parseInt(inputDateTab[0]), Integer.parseInt(inputDateTab[1]), Integer.parseInt(inputDateTab[2]), 0, 0);
+    }
 
     @GetMapping("")
     public String receptionist(Model model) {
@@ -43,5 +58,39 @@ public class ReceptionistController {
     public String delete(@PathVariable String id) {
         messageService.deleteById(id);
         return "redirect:/receptionist/messages";
+    }
+
+    @GetMapping("/visits")
+    public String getFormVisits(Model model){
+        model.addAttribute("dentistList", userService.findAllDoctor());
+        return "receptionist/visits/form";
+    }
+
+    @PostMapping("/visit/register")
+    public String postRegisterWeek(Model model, @RequestParam String date, @RequestParam String dentist){
+        LocalTime open = contactDataService.findAll().get(0).getOpen();
+        int defoultTimeVisit = 30;
+        model.addAttribute("dentistList", userService.findAllDoctor());
+        model.addAttribute("visitsCalendar", visitsCalendarService.findByDoctorId(dentist));
+        model.addAttribute("today", getDateFromInputDate(date));
+        model.addAttribute("dayOfWeek", getDateFromInputDate(date).getDayOfWeek().getValue());
+        model.addAttribute("difrence", defoultTimeVisit);
+        model.addAttribute("open", open);
+        return "receptionist/visits/calendar";
+    }
+
+    @GetMapping("/visit/register/add/{id}")
+    public String addVisit(@PathVariable String id,Model model){
+        model.addAttribute("patientList",userService.findAllPatient());
+        model.addAttribute(visitsCalendarService.findById(Long.parseLong(id)));
+        return "receptionist/visits/add";
+    }
+
+    @PostMapping("/visit/register/add/{id}")
+    public String updateVisitCalendarPatientId(@PathVariable String id, @RequestParam String patient){
+        VisitsCalendar visit = visitsCalendarService.findById(Long.parseLong(id));
+        visit.setPatient(userService.findById(patient));
+        visitsCalendarService.addVisit(visit);
+        return "patient/register/added";
     }
 }

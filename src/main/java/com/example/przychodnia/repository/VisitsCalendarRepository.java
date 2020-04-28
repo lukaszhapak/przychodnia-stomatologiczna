9ccd5a35c8df7c377752c3812197f70555a8a3b9
b.jakubowski <bartakj141@wp.pl>
2020-04-28 12:10:25 +0200

    add new query in visit calendar

--- src/main/java/com/example/przychodnia/repository/VisitsCalendarRepository.java
+++ src/main/java/com/example/przychodnia/repository/VisitsCalendarRepository.java
@@ -2,8 +2,10 @@ package com.example.przychodnia.repository;
 
 import com.example.przychodnia.entity.VisitsCalendar;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface VisitsCalendarRepository extends JpaRepository<VisitsCalendar, Long> {
 
@@ -15,4 +17,13 @@ public interface VisitsCalendarRepository extends JpaRepository<VisitsCalendar, 
             nativeQuery = true)
     Iterable<VisitsCalendar> findByIdDoctor(@Param("doctor") Long doctor);
 
+    @Query(value = "SELECT count(*) FROM visits_calendar WHERE doctor_id = :doctor and date = :date and patient_id > 0",
+            nativeQuery = true)
+    long findByDoctorAndDateCount(@Param("doctor") Long doctor, @Param("date") String date);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM visits_calendar WHERE date = :date AND doctor_id = :doctor",
+            nativeQuery = true)
+    void deleteByDateAndDoctor(@Param("date") String date, @Param("doctor") Long doctor);
 }
